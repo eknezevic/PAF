a13d4f260c05b1ca2c55781c4069183d964baf34
Eva Knezevic <eknezevic@pmfst.hr>
2023-04-01 14:59:23 +0200

    vj3

---
+++ Vjezbe/Vjezbe_4/gibanje.py
@@ -0,0 +1,8 @@
+import particle as prt
+import matplotlib.pyplot as plt
+
+p1=prt.Particle()
+p1.init(100,45,0,0,0.01)
+print("Domet je {:.2f}m.".format(p1.range()))
+p1.plot_trajectory()
+p1.reset()
